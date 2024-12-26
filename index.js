68f38b5382866bcf9693981e1b75507a41a5e7bc
Zdrave7o <zzgzdravko@gmail.com>
2024-12-26 13:07:32 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 let a;
 let b;
 let c;
+let price;
 let amount;
 let m3;
 
+    //IF THE USER TYPES DECIMAL NUMBER AND DOESN'T USE "." THE PROGRAM WOULDN'T WORK!!!
+
 document.getElementById("submit").onclick = function(){
+    
     a = document.getElementById("a").value;
     a = Number(a);
     a = a * 0.01;
@@ -23,6 +27,18 @@ document.getElementById("submit").onclick = function(){
     amount = Number(amount);
     console.log(amount, typeof amount);
 
+    price = document.getElementById("price").value;
+    price = Number(price);
+    console.log(price, typeof price);
+
     m3 = a * b * c * amount;
-    document.getElementById("result").textContent = `Кубатурата е ${m3} м3`
+    price = price * m3;
+
+    price = price.toFixed(2);
+    m3 = m3.toFixed(2);
+
+    //price = Math.floor(price);
+
+    document.getElementById("result").textContent = `Кубатура: ${m3} м3`;
+    document.getElementById("priceElement").textContent = `Цена: ${price} лв.`;
 }
