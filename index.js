92c8c05b9fe646d9dfd7af5fa4cac14ede0c39c1
Zdrave7o <zzgzdravko@gmail.com>
2025-04-01 16:17:42 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,29 +5,33 @@ let price;
 let amount;
 let m3;
 
-    //IF THE USER TYPES DECIMAL NUMBER AND DOESN'T USE "." THE PROGRAM WOULDN'T WORK!!!
-
 document.getElementById("submit").onclick = function(){
     
     a = document.getElementById("a").value;
+    a = a.replace("," , ".")
+    console.log(a);
     a = Number(a);
     a = a * 0.01;
     console.log(a, typeof a);
 
     b = document.getElementById("b").value;
+    b = b.replace("," , ".")
     b = Number(b);
     b = b * 0.01;
     console.log(b, typeof b);
 
     c = document.getElementById("c").value;
+    c = c.replace("," , ".")
     c = Number(c);
     console.log(c, typeof c);
 
     amount = document.getElementById("amount").value;
+    amount = amount.replace("," , ".")
     amount = Number(amount);
     console.log(amount, typeof amount);
 
     price = document.getElementById("price").value;
+    price = price.replace("," , ".")
     price = Number(price);
     console.log(price, typeof price);
 
@@ -35,7 +39,12 @@ document.getElementById("submit").onclick = function(){
     price = price * m3;
 
     price = price.toFixed(2);
-    m3 = m3.toFixed(2);
+    m3 = m3.toFixed(3);
+
+    if(document.querySelector("#includeDDS").checked){
+        price *= 1.20;
+         
+    }
 
     //price = Math.floor(price);
 
